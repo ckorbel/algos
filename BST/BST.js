dcb88c45e53ece3fe365809cfed653fe3396d78a
Christopher Korbel <ckorbel1@gmail>
2020-06-29 08:06:50 -0700

    BST start

---
+++ BST/BST.js
@@ -0,0 +1,52 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.right = null;
+    this.left = null;
+    this.count = 0;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insertNode(val) {
+    const newNode = new Node(val);
+
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    let current = this.root;
+
+    const addSide = (side) => {
+      if (!current[side]) {
+        current[side] = newNode;
+        return this;
+      }
+      current = current[side];
+    };
+
+    while (true) {
+      if (val === current.val) {
+        current.count++;
+        return this;
+      }
+      if (val < current.val) {
+        addSide("left");
+      } else {
+        addSide("right");
+      }
+    }
+  }
+}
+
+let tree = new BST();
+tree.insertNode(10);
+tree.insertNode(7);
+tree.insertNode(42);
+tree.insertNode(2);
+console.log(tree);
