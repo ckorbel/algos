d65adeb01670d0fc06b19dcb4d17e99348af810a
Christopher Korbel <ckorbel1@gmail>
2020-08-01 17:37:24 -0700

    remove DLL

--- LinkedList/DLL.js
+++ LinkedList/DLL.js
@@ -45,7 +45,7 @@ class DLL {
   }
 
   // insert at beginning
-  shift(val) {
+  unshift(val) {
     const newNode = new Node(val);
     if (!this.head) {
       return null;
@@ -56,8 +56,8 @@ class DLL {
     this.length++;
   }
 
-  // add an beginning
-  unshift() {
+  // remove from beginning
+  shift() {
     if (!this.head) {
       return null;
     }
@@ -69,16 +69,98 @@ class DLL {
     this.length--;
     return oldHead;
   }
+
+  get(index) {
+    if (!this.head || index < 0 || index > this.length) {
+      return null;
+    }
+    // start from back or front depending on which index is closer to
+    const mid = Math.floor(this.length / 2);
+    if (index < mid) {
+      let count = 0;
+      let current = this.head;
+      while (count !== index) {
+        current = current.next;
+        count++;
+      }
+      return current;
+    } else {
+      let count = this.length - 1;
+      let current = this.tail;
+      while (count !== index) {
+        current = current.prev;
+        count--;
+      }
+      return current;
+    }
+  }
+
+  // insert at specific index
+  set(index, val) {
+    const foundNode = this.get(index);
+    if (foundNode !== null) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
+
+  insert(index, val) {
+    if (index < 0 || index > this.length) {
+      return false;
+    }
+    if (index === 0) {
+      return this.unshift(val);
+    }
+
+    if (index === this.length) {
+      return this.push(val);
+    }
+    const newNode = new Node(val);
+    const beforeNode = this.get(index - 1);
+    const afterNode = beforeNode.next;
+    beforeNode.next = newNode;
+    newNode.prev = beforeNode;
+    newNode.next = afterNode;
+    afterNode.prev = newNode;
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (!this.head) {
+      return null;
+    }
+
+    if (index === 0) {
+      return this.shift();
+    }
+
+    if (index === this.length) {
+      return this.pop();
+    }
+
+    const foundNode = this.get(index);
+    const beforeNode = foundNode.prev;
+    const afterNode = foundNode.next;
+    console.log(beforeNode, afterNode);
+    beforeNode.next = afterNode;
+    afterNode.prev = beforeNode;
+    this.length--;
+    return foundNode;
+  }
 }
 
 const dll = new DLL();
+dll.push(0);
 dll.push(1);
 dll.push(2);
 dll.push(3);
 // console.log(dll);
-dll.pop();
+// dll.pop();
 // console.log(dll);
-dll.shift("I AM first");
-console.log(dll);
-dll.unshift();
+// dll.unshift("I AM first");
+// console.log(dll);
+// dll.unshift();
+dll.remove(2);
 console.log(dll);
