f9e8b0e562669f85ddc19ea1f3bfcb2855062f6f
Christopher Korbel <ckorbel1@gmail>
2020-10-16 10:06:23 -0700

    memoization

--- recursion/fib.js
+++ recursion/fib.js
@@ -15,6 +15,7 @@ function fib(n, memo = []) {
   if (n <= 2) {
     return 1;
   }
+
   const res = fib(n - 1, memo) + fib(n - 2, memo);
   memo[n] = res;
   return res;
--- recursion/fib.py
+++ recursion/fib.py
@@ -5,3 +5,19 @@ def fib(n):
 
 
 print(fib(8))
+
+
+memo = {}
+
+
+def fib_memo(n):
+    if n in memo:
+        return memo[n]
+    if n <= 2:
+        return 1
+    res = fib(n - 1) + fib(n - 2)
+    memo[n] = res
+    return res
+
+
+print(fib_memo(45))
