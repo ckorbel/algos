8614c9e5af946c83dcc31dc480c391d04b9b30f6
Christopher Korbel <ckorbel1@gmail>
2021-02-11 10:50:04 -0800

    can sum dynamic

--- dynamic/can_sum.py
+++ dynamic/can_sum.py
@@ -1,3 +1,5 @@
+## recursion
+
 def can_sum(target, nums, memo = {}):
   if target in memo: 
     return memo[target]
@@ -13,5 +15,21 @@ def can_sum(target, nums, memo = {}):
       return True
   memo[target] = False
   return False
+
+print(can_sum(300, [7, 14]))
+
+##table
+# 0(n * m) n being target sum nums being length of nums array
+def can_sum(target, nums):
+  table = [False] * (target + 1)
+  table[0] = True
+  for i in range(len(table)):
+    if table[i]:
+      for num in nums:
+        if (i + num) <= len(table) - 1:
+          table[i + num] = True
+  return table[target]
+
+
 
 print(can_sum(300, [7, 14]))
