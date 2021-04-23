5994a6de8573b86bf7c4ea63119aeb89ebea5cf9
Christopher Korbel <ckorbel1@gmail>
2021-04-22 17:32:17 -0700

    back tracking

---
+++ backtracking/generate_parenthesis.py
@@ -0,0 +1,18 @@
+class Solution:
+    def back_track(self, output, current_string, open, closed, max):
+        if len(current_string) == (max * 2): 
+            output.append(current_string)
+            return 
+        
+        # rules
+        if open < max: 
+            self.back_track(output, current_string + "(", open + 1, closed, max)
+        
+        if closed < open: 
+            self.back_track(output, current_string + ")", open, closed + 1, max)
+            
+    
+    def generateParenthesis(self, n: int) -> List[str]:
+        output = []
+        self.back_track(output, "", 0, 0, n)
+        return output
---
+++ backtracking/info.md
@@ -0,0 +1,2 @@
+back tracking is generally used for problems like:
+: generate all combinations of x,y,z
