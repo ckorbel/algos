afcb2a0100423bf43ecd65e75de46a9c853b1252
Christopher Korbel <ckorbel@Christophers-MacBook-Pro.local>
2021-06-27 19:04:19 -0700

    py

---
+++ python/logging.py
@@ -0,0 +1,6 @@
+import logging
+logging.debug("This is debug")
+logging.info("this is info")
+logging.warning('this is warning')
+logging.error("this is error")
+logging.critical("this is critical")
